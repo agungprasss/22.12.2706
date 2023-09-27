35587e786be223797c0247a6c5860698d76be17a
agungprasss <amrullahagungprasetya@students.amikom.ac.id>
2023-09-27 16:22:23 +0700

    update

---
+++ app/src/main/java/com/example/loginactyvity.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectbp_2706.R
+
+class loginactyvity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginactyvity)
+    }
+}
